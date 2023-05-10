8583d17662616eb7c2be504d412af58a561e7262
Muhammed Erdogan <muhammedea33@gmail.com>
2023-05-10 22:11:01 +0300

    change: reordering class

--- Assets/Scripts/Conroller/GameController.cs
+++ Assets/Scripts/Conroller/GameController.cs
@@ -4,64 +4,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+namespace Controller
 {
-    private void OnEnable()
+    public class GameController : MonoBehaviour
     {
-        EventManager.StartListening(EventKeys.OnGameStarted, Init);
-        EventManager.StartListening(EventKeys.OnGamePaused, OnGamePaused);
-        EventManager.StartListening(EventKeys.OnGameResumed, OnGameResumed);
-        EventManager.StartListening(EventKeys.LevelLoaded, OnLevelLoaded);
-        EventManager.StartListening(EventKeys.TowerCompleted, OnTowerCompleted);
-        EventManager.StartListening(EventKeys.OnPlayerUnitCountChange, OnPlayerUnitChange);
-    }
-
-    private void OnDisable()
-    {
-        EventManager.StopListening(EventKeys.OnGameStarted, Init);
-        EventManager.StopListening(EventKeys.OnGamePaused, OnGamePaused);
-        EventManager.StopListening(EventKeys.OnGameResumed, OnGameResumed);
-        EventManager.StopListening(EventKeys.LevelLoaded, OnLevelLoaded);
-        EventManager.StopListening(EventKeys.TowerCompleted, OnTowerCompleted);
-        EventManager.StopListening(EventKeys.OnPlayerUnitCountChange, OnPlayerUnitChange);
-    }
-
-    private void Init(object[] __)
-    {
-        Time.timeScale = 1.0f;
-    }
-
-    private void OnGamePaused(object[] __)
-    {
-        Time.timeScale = 0f;
-    }
-
-    private void OnGameResumed(object[] __)
-    {
-        Time.timeScale = 1.0f;
-    }
-
-    private void OnLevelLoaded(object[] __)
-    {
-        Time.timeScale = 1.0f;
-    }
-
-    private void OnPlayerUnitChange(object[] obj)
-    {
-        var count = (int)obj[0];
-        if (count <= 0)
+        private void OnEnable()
         {
-            EventManager.TriggerEvent(EventKeys.LevelFailed, null);
-            Time.timeScale = 0;
+            EventManager.StartListening(EventKeys.OnGameStarted, Init);
+            EventManager.StartListening(EventKeys.OnGamePaused, OnGamePaused);
+            EventManager.StartListening(EventKeys.OnGameResumed, OnGameResumed);
+            EventManager.StartListening(EventKeys.LevelLoaded, OnLevelLoaded);
+            EventManager.StartListening(EventKeys.TowerCompleted, OnTowerCompleted);
+            EventManager.StartListening(EventKeys.OnPlayerUnitCountChange, OnPlayerUnitChange);
         }
-    }
 
-    private void OnTowerCompleted(object[] __)
-    {
-        this.DelayedAction(5f, () =>
+        private void OnDisable()
         {
-            EventManager.TriggerEvent(EventKeys.LevelCompleted, null);
+            EventManager.StopListening(EventKeys.OnGameStarted, Init);
+            EventManager.StopListening(EventKeys.OnGamePaused, OnGamePaused);
+            EventManager.StopListening(EventKeys.OnGameResumed, OnGameResumed);
+            EventManager.StopListening(EventKeys.LevelLoaded, OnLevelLoaded);
+            EventManager.StopListening(EventKeys.TowerCompleted, OnTowerCompleted);
+            EventManager.StopListening(EventKeys.OnPlayerUnitCountChange, OnPlayerUnitChange);
+        }
+
+        private void Init(object[] __)
+        {
+            Time.timeScale = 1.0f;
+        }
+
+        private void OnGamePaused(object[] __)
+        {
             Time.timeScale = 0f;
-        }, out _);
+        }
+
+        private void OnGameResumed(object[] __)
+        {
+            Time.timeScale = 1.0f;
+        }
+
+        private void OnLevelLoaded(object[] __)
+        {
+            Time.timeScale = 1.0f;
+        }
+
+        private void OnPlayerUnitChange(object[] obj)
+        {
+            var count = (int)obj[0];
+            if (count <= 0)
+            {
+                EventManager.TriggerEvent(EventKeys.LevelFailed, null);
+                Time.timeScale = 0;
+            }
+        }
+
+        private void OnTowerCompleted(object[] __)
+        {
+            this.DelayedAction(5f, () =>
+            {
+                EventManager.TriggerEvent(EventKeys.LevelCompleted, null);
+                Time.timeScale = 0f;
+            }, out _);
+        }
     }
 }
--- Assets/Scripts/Manager/LevelManager.cs
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -2,73 +2,76 @@ using UnityEngine;
 using System.Collections.Generic;
 using Manager;
 
-public class LevelManager : MonoBehaviour
+namespace Manager
 {
-    [SerializeField] private List<LevelModel> levels;
-    [SerializeField] private Transform levelParents;
-
-    private LevelModel _activeLevel;
-    private int _currentLevel;
-
-    private void Awake()
+    public class LevelManager : MonoBehaviour
     {
-        _currentLevel = SaveManager.LoadInt(SaveManager.KEY_LEVEL_INDEX);
+        [SerializeField] private List<LevelModel> levels;
+        [SerializeField] private Transform levelParents;
 
-        for (int i = levels.Count - 1; i >= 0; i--)
+        private LevelModel _activeLevel;
+        private int _currentLevel;
+
+        private void Awake()
         {
-            levels[i].CloseLevel();
+            _currentLevel = SaveManager.LoadInt(SaveManager.KEY_LEVEL_INDEX);
+
+            for (int i = levels.Count - 1; i >= 0; i--)
+            {
+                levels[i].CloseLevel();
+            }
+
+            _activeLevel = levels[GetLevelIndex()];
+            _activeLevel.SetupLevel();
         }
 
-        _activeLevel = levels[GetLevelIndex()];
-        _activeLevel.SetupLevel();
-    }
-
-    private void OnEnable()
-    {
-        EventManager.StartListening(EventKeys.OnGameStarted, LoadLevel);
-        EventManager.StartListening(EventKeys.OnNextLevelRequest, NextLevel);
-    }
-
-    private void OnDisable()
-    {
-        EventManager.StopListening(EventKeys.OnGameStarted, LoadLevel);
-        EventManager.StopListening(EventKeys.OnNextLevelRequest, NextLevel);
-    }
-
-    private void LoadLevel(object[] obj = null)
-    {
-        int index = GetLevelIndex();
-        if(_activeLevel != null)
+        private void OnEnable()
         {
-            _activeLevel.gameObject.SetActive(false);
+            EventManager.StartListening(EventKeys.OnGameStarted, LoadLevel);
+            EventManager.StartListening(EventKeys.OnNextLevelRequest, NextLevel);
         }
 
-        _activeLevel = levels[index];
-        _activeLevel.SetupLevel();
-
-        EventManager.TriggerEvent(EventKeys.LevelLoaded, new object[] { _currentLevel, _activeLevel.PlayerStartPos });
-        SaveManager.SaveInt(SaveManager.KEY_LEVEL_INDEX, _currentLevel);
-    }
-
-    public void NextLevel(object[] obj = null)
-    {
-        _currentLevel++;
-        GetLevelIndex();
-        LoadLevel();
-    }
-
-    public void ReplayThisLevel()
-    {
-        LoadLevel();
-    }
-
-    private int GetLevelIndex()
-    {
-        if (_currentLevel < levels.Count)
+        private void OnDisable()
         {
-            return _currentLevel;
+            EventManager.StopListening(EventKeys.OnGameStarted, LoadLevel);
+            EventManager.StopListening(EventKeys.OnNextLevelRequest, NextLevel);
         }
 
-        return Random.Range(0, levels.Count);
+        private void LoadLevel(object[] obj = null)
+        {
+            int index = GetLevelIndex();
+            if (_activeLevel != null)
+            {
+                _activeLevel.gameObject.SetActive(false);
+            }
+
+            _activeLevel = levels[index];
+            _activeLevel.SetupLevel();
+
+            EventManager.TriggerEvent(EventKeys.LevelLoaded, new object[] { _currentLevel, _activeLevel.PlayerStartPos });
+            SaveManager.SaveInt(SaveManager.KEY_LEVEL_INDEX, _currentLevel);
+        }
+
+        public void NextLevel(object[] obj = null)
+        {
+            _currentLevel++;
+            GetLevelIndex();
+            LoadLevel();
+        }
+
+        public void ReplayThisLevel()
+        {
+            LoadLevel();
+        }
+
+        private int GetLevelIndex()
+        {
+            if (_currentLevel < levels.Count)
+            {
+                return _currentLevel;
+            }
+
+            return Random.Range(0, levels.Count);
+        }
     }
 }
